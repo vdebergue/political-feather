3abd4315fb74f3367e0371625d8dbcbceb1938b5
Vincent Debergue <vdebergue@gmail.com>
2014-01-23 22:25:00 +0100

    add Sliding Buffer

---
+++ app/actors/analysis/Sentiment.scala
@@ -0,0 +1,8 @@
+package actors.analysis
+
+/**
+ * Created by vince on 23/01/14.
+ */
+class Sentiment {
+
+}
---
+++ app/models/analysis/SlidingBuffer.scala
@@ -0,0 +1,53 @@
+package models.analysis
+
+import org.joda.time.{Days, Period, DateTime}
+import scala.collection.mutable
+
+/**
+ * Created by vince on 23/01/14.
+ */
+trait WithDate {
+  def date: DateTime
+}
+
+class SlidingBuffer[T <: WithDate](val timespan: Period = Period.days(1)) {
+
+  var lastDate : DateTime = _
+  val buffer = mutable.Buffer[T]()
+
+  private def clean() {
+    buffer.headOption.map(h =>
+      if(lastDate.minus(timespan).isAfter(h.date)) {
+        buffer.remove(0)
+        clean()
+      }
+    )
+  }
+
+  def push(t: T) {
+    buffer += t
+    lastDate = t.date
+    clean()
+  }
+
+  def toList = {
+    buffer.toList
+  }
+
+}
+
+object SlidingBuffer {
+
+  def oneDay[T <: WithDate] : SlidingBuffer[T] = {
+    new SlidingBuffer[T]()
+  }
+
+  def oneWeek[T <: WithDate] : SlidingBuffer[T] = {
+    new SlidingBuffer[T](Period.weeks(1))
+  }
+
+  def apply[T <: WithDate](p : Period) = {
+    new SlidingBuffer[T](p)
+  }
+
+}
